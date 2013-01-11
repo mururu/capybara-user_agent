12f434373b8dd29b21a28b31cf395135d1e2f61f
mururu <yuki@gnnk.net>
2013-01-11 10:49:14 +0900

    Implement set_custom_user_agent

--- lib/capybara/user_agent/dsl.rb
+++ lib/capybara/user_agent/dsl.rb
@@ -2,7 +2,11 @@ module Capybara
   module UserAgent
     module DSL
       def set_user_agent(type)
-        Capybara.current_session.driver.header('User-Agent', Capybara::UserAgent.user_agents[type])
+        set_custom_user_agent(Capybara::UserAgent.user_agents[type])
+      end
+
+      def set_custom_user_agent(user_agent)
+        Capybara.current_session.driver.header('User-Agent', user_agent)
       end
     end
   end
